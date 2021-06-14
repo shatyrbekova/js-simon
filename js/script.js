c046e091f615c079b7ef1ddea4045d24c6ff46db
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-14 14:26:25 +0200

    STRUTTURA

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+//Descrizione:
+// Un alert() espone 5 numeri generati casualmente.
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, 
+//tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e 
+//quali dei numeri da indovinare sono stati individuati.
+
+
+
+document.addEventListener('DOMContentLoaded'), function (){
+
+    // THE FIRST STEP:
+    //Creo un alert che contiene 5 numeri che deve apparire sulla pagina per 30 secondi; 
+
+    
+    //THE SECOND STEP:
+    //In seguito, creo for oppure while (var i =0; i<=5; i++)
+    //con parseInt (prompt ('Inserisci i numeri che hai visto precedentemente'));
+    //THE THIRD STEP: PROBLEMA 1.
+    //Come individuare quanti e quali dei numeri sono stati individuati???
+
+
+}
